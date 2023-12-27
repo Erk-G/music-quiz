22854ed14d5369a0b2e8eb386db56c514c9a8543
Erik <erkgarciae@gmail.com>
2023-12-27 00:47:51 -0500

    Removing files after planning

--- src/convertTXT.js
+++
@@ -1,2 +0,0 @@
-// To make this re-usable I want this app to read a json-like txt that contains all the video links for the questions, sorted by category
-// This will be a helper function to read those txts
