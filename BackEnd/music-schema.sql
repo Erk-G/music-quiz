6257b68e212109b20d2ad9b34e229eed659c12f4
Erik <erkgarciae@gmail.com>
2024-01-03 21:59:04 -0500

    Decided to make the url the PRIMARY KEY

--- BackEnd/music-schema.sql
+++ BackEnd/music-schema.sql
@@ -2,11 +2,10 @@ DROP TABLE IF EXISTS songs;
 DROP TABLE IF EXISTS genre;
 
 CREATE TABLE songs(
-    id SERIAL PRIMARY KEY,
+    url TEXT PRIMARY KEY,
     difficulty TEXT NOT NULL,
     genre TEXT NOT NULL,
     song_title TEXT NOT NULL,
     song_origin TEXT NOT NULL,
-    url TEXT UNIQUE NOT NULL,
     player_targets TEXT
 );
