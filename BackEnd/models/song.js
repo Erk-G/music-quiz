7b9e1d490be324b79058daec391a2bfd67a6f96b
Erik <erkgarciae@gmail.com>
2024-01-03 22:50:17 -0500

    song model V1 complete

--- BackEnd/models/song.js
+++ BackEnd/models/song.js
@@ -3,10 +3,55 @@ const db=require("../db");
 class Song{
     /**
      * Given a difficulty and genre return an array of songs of that difficulty
+     * =>{[{url,difficulty,genre,song_title,song_origin,player_targets}...]}
      */
+    static async getSongs(genre,difficulty){
+        const songRes=await db.query(
+            `SELECT url,
+                    difficulty,
+                    genre,
+                    song_title,
+                    song_origin,
+                    player_targets
+                    FROM songs
+                    WHERE genre=$1 AND difficulty=$2`,[genre,difficulty]);
+        return songRes.rows;
+    }
 
 
     /**
-     * Putting a song into the database
+     * Putting a new song into the database
+     * INPUT:
+     * {url,difficulty,genre,song_title,song_origin,player_targets}
+     * => {url,difficulty,genre,song_title,song_origin,player_targets}
      */
+
+    static async newSong(data){
+        const songRes=await db.query(
+            `INSERT INTO songs(
+                url,
+                difficulty,
+                genre,
+                song_title,
+                song_origin,
+                player_targets)
+                VALUES ($1,$2,$3,$4,$5,$6)
+                RETURNING url,
+                          difficulty,
+                          genre,
+                          song_title,
+                          song_origin,
+                          player_targets`,
+            [
+                data.url,
+                data.difficulty,
+                data.genre,
+                data.song_title,
+                data.song_origin,
+                data.player_targets
+            ]
+        );
+
+        return songRes.rows[0];
+    }
 }
--- README.md
+++ README.md
@@ -46,3 +46,5 @@ With this update the front end is pretty much done.  useEffect is leftover as wh
 My next steps are adding in Player score, creating a psql db, an api to interact with the db and maybe a helper api for the front end.
 
 Creating the schema for my database I wonder if it would be better to have a seperate table for genres.
+
+Decided to make url the PRIMARY KEY for songs since it is the only piece of information that I know shouldn't ever be repeated twice. It's difficult to do that for data like song_title because some songs have longer titles or in different languages or maybe it's just spelled wrong. Caveat is there is multiple youtube videos of the same song but this is the best I can do. I also made it a PRIMARY KEY because I realize I don't have a need for a numerical id since my current plan is to grab an array of songs from genre and difficulty and Math.rand choose from that array.
